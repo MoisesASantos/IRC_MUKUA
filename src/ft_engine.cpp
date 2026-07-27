6cc8e4f611efbfb12c7f31ea32c7b7251cdcd75c
Moises Santos <mosantos@c2r7s5.42luanda.com>
2026-07-27 15:10:25 +0100

    add new change on ft_execute_engine function

--- src/ft_engine.cpp
+++ src/ft_engine.cpp
@@ -6,7 +6,7 @@
 /*   By: mosantos <mosantos@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/27 12:31:22 by mosantos          #+#    #+#             */
-/*   Updated: 2026/07/27 15:06:42 by mosantos         ###   ########.fr       */
+/*   Updated: 2026/07/27 15:08:44 by mosantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,19 @@ void    ft_execute_server(Server& server, std::string port)
     Client*     client;
     std::string command;
     int ret;
-    pollfd& pfd;
 
     server.ServerInit(port);
     while (server.IsRunning())
     {
         ret = poll(&server.GetPollFd(0), server.GetFdCount(), 0);
-
         if (ret <= 0)
             continue;
+        
         for (size_t i = 0; i < server.GetFdCount(); i++)
         {
-            pfd = server.GetPollFd(i);
+            pollfd& pfd = server.GetPollFd(i);
             
-            if (pfd.revents & POLLIN)
+            if (!(pfd.revents & POLLIN))
                 continue;
             if (pfd.fd == server.GetServerSocketFd())
                 server.AcceptNewClient();
